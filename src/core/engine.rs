6e35ffefdf2ce88afeff047c77c49c4ad73edca8
Emmanuel Baah <beks@BEKs-3.local>
2025-11-09 08:48:54 +0200

    added event handling basic structures

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -4,8 +4,14 @@
 //!
 //! This module orchestrates and manages all other components
 //! and their interaction
-
-use winit::application::ApplicationHandler;
+use pollster;
+use winit::{
+    application::ApplicationHandler,
+    event::{ElementState, KeyEvent, WindowEvent},
+    event_loop::ActiveEventLoop,
+    keyboard::{Key, NamedKey},
+    window::WindowId,
+};
 
 use super::super::{
     renderer::{Renderer, RendererBackends},
@@ -26,6 +32,9 @@ pub struct Engine {
     height: u32,
     width: u32,
 
+    // renderer specific config
+    backend: RendererBackends,
+
     // timing details
     last_update: Option<Instant>,
     accumulator: f32,
@@ -36,7 +45,7 @@ pub struct Engine {
 // game engine.
 //
 impl Engine {
-    pub fn new(title: &str, width: u32, height: u32) -> Self {
+    pub fn new(title: &str, width: u32, height: u32, backend: RendererBackends) -> Self {
         Self {
             window: None,
             renderer: None,
@@ -45,6 +54,7 @@ impl Engine {
             height: height,
             last_update: None,
             accumulator: 0.0,
+            backend: backend,
         }
     }
 
@@ -64,15 +74,82 @@ impl Engine {
 // === winit ApplicationHandler for Engine
 impl ApplicationHandler for Engine {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        todo!()
+        // create window
+        let config = WindowConfig {
+            title: self.title.clone(),
+            width: self.width,
+            height: self.height,
+            resizable: true,
+            vsync: true,
+            fullscreen: false,
+        };
+        match Window::create(config, event_loop) {
+            Ok(window) => {
+                println!("Window created successfully!");
+                self.window = Some(window);
+            }
+            Err(e) => {
+                eprintln!("Failed to create window: {:?}", e);
+                event_loop.exit();
+            }
+        }
+        // create renderer
+        if let Some(window) = &self.window {
+            self.renderer = pollster::block_on(async {
+                Some(
+                    Renderer::new(window.inner(), self.backend.clone())
+                        .await
+                        .ok()?,
+                )
+            });
+        }
+
+        self.last_update = Some(Instant::now());
     }
 
     fn window_event(
         &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: winit::window::WindowId,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
         event: winit::event::WindowEvent,
     ) {
-        todo!()
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        logical_key: key,
+                        state: ElementState::Pressed,
+                        ..
+                    },
+                ..
+            } => match key {
+                Key::Named(NamedKey::Escape) => {
+                    println!("Escape key pressed, closing...");
+                    event_loop.exit()
+                }
+                _ => (),
+            },
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
     }
 }
