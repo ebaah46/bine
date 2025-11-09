cfb736f81ca197f74fb8499530f088aea791ea58
Emmanuel Baah <beks@BEKs-3.local>
2025-11-09 08:48:23 +0200

    removed demo app initializations from main

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,10 @@
-use bine::window::{Window, WindowConfig};
+use bine::core::Engine;
 
-use winit::{
-    application::ApplicationHandler,
-    event::{ElementState, KeyEvent, WindowEvent},
-    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
-    keyboard::{Key, NamedKey},
-    window::WindowId,
-};
-
-#[derive(Default)]
-struct App {
-    window: Option<Window>,
-}
-
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        if self.window.is_none() {
-            let config = WindowConfig::new("Demo App", 1980, 1600, true);
-            match Window::create(config, event_loop) {
-                Ok(window) => {
-                    println!("Window created successfully!");
-                    self.window = Some(window);
-                }
-                Err(e) => {
-                    eprintln!("Failed to create window: {:?}", e);
-                    event_loop.exit();
-                }
-            }
-        }
-    }
-
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
-                event_loop.exit();
-            }
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        logical_key: key,
-                        state: ElementState::Pressed,
-                        ..
-                    },
-                ..
-            } => match key {
-                Key::Named(NamedKey::Escape) => {
-                    println!("Escape key pressed, closing...");
-                    event_loop.exit()
-                }
-                _ => (),
-            },
-            WindowEvent::RedrawRequested => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-
-                // Draw.
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
-            }
-            _ => (),
-        }
-    }
-}
+use winit::event_loop::{ControlFlow, EventLoop};
 
 fn main() {
     // Demo app to test created modules
-    //
+    env_logger::init();
     let event_loop = EventLoop::new().unwrap();
 
     // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
@@ -85,7 +15,11 @@ fn main() {
     // This is ideal for non-game applications that only update in response to user
     // input, and uses significantly less power/CPU time than ControlFlow::Poll.
     event_loop.set_control_flow(ControlFlow::Wait);
-
-    let mut app = App::default();
-    let _ = event_loop.run_app(&mut app);
+    let mut engine = Engine::new(
+        "Demo Game",
+        1980,
+        1680,
+        bine::renderer::RendererBackends::Metal,
+    );
+    let _ = event_loop.run_app(&mut engine);
 }
